ae631bd505570c0d528e2ae0d80b0223ebfccd1c
boss <75540581+Boss22341231@users.noreply.github.com>
2024-11-27 22:14:57 +0700

    Update Aleart

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -56,15 +56,15 @@ export default function Home() {
   return (
     <div className="">
       <Header score={score} bestScore={bestScore}/>
-      <dialog id="dialog" className="border-4 p-6 text-center rounded-lg shadow-lg z-20 fixed top-[350px]">
+      <dialog id="dialog" className="border-4 p-6 text-center rounded-lg shadow-lg z-20 fixed top-[35%]">
         <h1 className="text-3xl font-semibold">🎉Congratulations🎉</h1>
         <h2 className="text-2xl font-semibold">You Won!</h2>
         <button onClick={() => closeDialog()} className="text-2xl mt-4 bg-red-500 text-white p-1 rounded-lg">Close</button>
       </dialog>
       <div className="grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 justify-center items-center gap-5 p-10">
         {heroData.map((hero) => (
-          <div onClick={() => handleClick(hero.id)} className="p-3 bg-[#ffffff] rounded-lg shadow-lg duration-200 cursor-pointer hover:scale-105" key={hero.id}>
-            <h2 className="text-3xl font-bold text-[#000000]">{hero.heroName}</h2>
+          <div onClick={() => handleClick(hero.id)} className="p-3 flex flex-col items-center bg-[#ffffff] rounded-lg shadow-lg duration-200 cursor-pointer hover:scale-105" key={hero.id}>
+            <h2 className="text-3xl w-full text-start font-bold text-[#000000]">{hero.heroName}</h2>
             <Image className="border-2 border-[#878787] rounded-lg" src={`/Heroes/${hero.src}`} width={300} height={300} alt="hero"/>
           </div>
         ))}
